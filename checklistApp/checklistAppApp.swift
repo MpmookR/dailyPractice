130db197a73d73bd26f0576cc285435f33c2b603
Mook <mpmookr@gmail.com>
2025-10-17 14:32:32 +0100

    Initial Commit

---
+++ checklistApp/checklistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  checklistAppApp.swift
+//  checklistApp
+//
+//  Created by Mook Rattana on 17/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct checklistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
